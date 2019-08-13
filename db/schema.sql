efc1cb126cb6b5f5a76b197b5088931ea83974ca
Yating(Esther) Su <yaringsu@foxmail.com>
2019-08-13 16:22:20 -0400

    Finished burger_db ,and  DB setup

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+create database burgers_db;
+use burgers_db;
+create table burgers(
+	id int(30) not null auto_increment,
+    burger_name varchar(80) ,
+    devoured boolean,
+    primary key(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+insert into burgers (burger_name, devoured) values ("Fancy island" , true);
+insert into burgers (burger_name, devoured) values ("Tall building" , false);
+insert into burgers (burger_name, devoured) values ("Magic hill" , false);
+insert into burgers (burger_name, devoured) values ("Unique cube" , true);
+insert into burgers (burger_name, devoured) values ("Burger normal" , false);
+insert into burgers (burger_name, devoured) values ("Little patato" , false);
