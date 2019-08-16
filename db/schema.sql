2be208dde14f5838a495da7abd93971e2d6c2021
Yating(Esther) Su <yaringsu@foxmail.com>
2019-08-15 23:14:17 -0400

    Edited .env settings

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ create database burgers_db;
 use burgers_db;
 
 create table burgers(
-    id int not null auto_increment,
+    id int(30) auto_increment not null,
     burger_name varchar(80),
     devoured boolean,
     primary key(id)
