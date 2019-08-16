d5c5c02a29ccbe91a062c36840819aecdb8129f6
Yating(Esther) Su <yaringsu@foxmail.com>
2019-08-15 23:02:31 -0400

    comment port

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 var mysql = require("mysql");
 var connection;
+
 if(process.env.JAWSDB_URL){
     console.log(process.env.JAWSDB_URL);
         connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
         connection = mysql.createConnection({
             host : "localhost",
-            port : 3306,
+          //  port : 3306,
             user : "root",
             password : "123456",
             database : "burgers_db"  
     });
-}
+};
 
 connection.connect(function(err,res){
     if(err) throw err;
